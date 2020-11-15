3ce60347595c45626f3e7a0d5603caa2b37a0034
leonid9191 <leonid0991@gmail.com>
2020-11-15 18:59:07 +0200

    optimization

--- dist/js/script.js
+++ dist/js/script.js
@@ -47,68 +47,42 @@ $(document).ready(function () {
         })
     });
 
-    function validateForms(form) {
-        $(form).validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                phone: "required",
-                email: {
-                    required: true,
-                    email: true
-                }
-            },
-            messages: {
-                name: {
-                    required: "Пожалуйста, введите свое имя",
-                    minlength: jQuery.validator.format("Введите {0} символа!")
-                },
-                phone: "Пожалуйста, введите свой номер телефона",
-                email: {
-                    required: "Пожалуйста, введите свою почту",
-                    email: "Неправильно введен адрес почты"
-                }
+    $('#consultation-form').validate();
+    $('#consultation form').validate({
+        rules: {
+            name: "required",
+            phone: "required",
+            email: {
+                required: true,
+                email: true
             }
-        });
-    };
-
-    validateForms('#consultation-form');
-    validateForms('#consultation form');
-    validateForms('#order form');
+        },
+        messages: {
+            name: "Пожалуйста введите свое имя",
+            phone: "Пожалуйста введите свой номер телефона",
+            email: {
+                required: "Пожалуйста введите email",
+                email: "Не правильно введен адрес почты"
+            }
+        }
+    });
+    $('#order form').validate();
 
     $('input[name=phone]').mask("+7 (999) 999-9999");
 
-    $('form').submit(function (e) {
+    $('form').submit(function(e) {
         e.preventDefault();
         $.ajax({
             type: "POST",
             url: "mailer/smart.php",
             data: $(this).serialize()
-        }).done(function () {
+        }).done(function() {
             $(this).find("input").val("");
             $('#consultation, #order').fadeOut();
             $('.overlay, #thanks').fadeIn('slow');
 
             $('form').trigger('reset');
         });
-        return false;
-    });
-
-    // Smooth scroll and pageup
-
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 1600) {
-            $('.pageup').fadeIn();
-        } else {
-            $('.pageup').fadeOut();
-        }
-    });
-
-    $("a[href^='#']").click(function () {
-        const _href = $(this).attr("href");
-        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
         return false;
     });
 }); 
